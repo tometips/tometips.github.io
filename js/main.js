62b8a147245d916052bf47353d2a7a481e9caf49
Josh Kelley <joshkel@gmail.com>
2014-05-13 23:06:16 -0400

    Sync with source.
    Version selector, configurable icon sizes.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,22 @@
+function locationHashNoQuery()
+{
+    return location.hash.replace(/\?.*/, '');
+}
+
+function currentQuery()
+{
+    var query = '';
+    query += versions.asQuery();
+    if (query) {
+        return '?' + query;
+    } else {
+        return '';
+    }
+}
+
 function scrollToId()
 {
-    var $hash = $(location.hash.replace(/\//g, '\\/'));
+    var $hash = $(locationHashNoQuery().replace(/\//g, '\\/'));
     if ($hash.length) {
         $("#content-container").scrollTop($("#content-container").scrollTop() + $hash.offset().top);
     }
@@ -16,6 +32,42 @@ function enableExpandCollapseAll()
         .click(function() {
             $($(this).attr('data-target')).find('.collapse').collapse($(this).hasClass('expand-all') ? 'show' : 'hide');
         });
+}
+
+function showCollapsed(html_id, disable_transitions)
+{
+    if (html_id[0] != '#') {
+        html_id = '#' + html_id;
+    }
+
+    // Hack: If requested, temporarily disable transitions.  Bootstrap's default
+    // transition is 0.35s, so we do just a bit longer.
+    // Based on http://stackoverflow.com/a/22428493/25507
+    if (disable_transitions) {
+        $(html_id).addClass('disable-transition');
+        setTimeout(function() { $(html_id).removeClass('disable-transition'); }, 400);
+    }
+
+    $(html_id).collapse('show');
+    // Hack: Update "collapsed" class, since Bootstrap doesn't seem to do it
+    // for us (unless, presumably, we use data-parent for full-blown accordion
+    // behavior, and I don't really want to do that).
+    $("[data-target=" + html_id + "]").removeClass('collapsed');
+}
+
+/**Gets the HTML IDs of currently expanded collapsed items. */
+function getExpandedIds()
+{
+    return $.map($(".collapse.in"), function(n, i) {
+        return n.id;
+    });
+}
+
+function expandIds(id_list, disable_transitions)
+{
+    for (var i = 0; i < id_list.length; i++) {
+        showCollapsed(id_list[i], disable_transitions);
+    }
 }
 
 function makeStickyHeader($header, $container)
@@ -41,6 +93,10 @@ function makeStickyHeader($header, $container)
         $sticky.css('width', $header.width());
     });
 }
+
+var options = {
+    imgSize: 48
+};
 
 ///Simplistic title-case function that capitalizes the beginning of every word.
 function toTitleCase(s)
@@ -86,7 +142,15 @@ Handlebars.registerHelper('toHtmlId', function(context, options) {
 });
 
 Handlebars.registerHelper('tag', function(context, options) {
-    return tome.tag;
+    return tome[versions.current].tag;
+});
+
+Handlebars.registerHelper('currentQuery', function(context, options) {
+    return currentQuery();
+});
+
+Handlebars.registerHelper('opt', function(opt_name) {
+    return options[opt_name];
 });
 
 // See http://stackoverflow.com/a/92819/25507
@@ -106,7 +170,7 @@ var talent_by_type_template = Handlebars.compile(
                 '<div class="panel-heading clickable">' +
                     '<h3 class="panel-title">' +
                         '<a data-toggle="collapse" data-target="#collapse-{{toHtmlId id}}">' +
-                            '<img width="64" height="64" src="img/talents/{{#if image}}{{image}}{{else}}{{toLowerCase short_name}}.png{{/if}}" onerror="talentImgError(this)">' + '{{name}}' +
+                            '<img width="{{opt "imgSize"}}" height="{{opt "imgSize"}}" src="img/talents/{{opt "imgSize"}}/{{#if image}}{{image}}{{else}}{{toLowerCase short_name}}.png{{/if}}" onerror="talentImgError(this)">' + '{{name}}' +
                         '</a>' +
                     '</h3>' +
                 '</div>' +
@@ -129,8 +193,8 @@ var talent_by_type_template = Handlebars.compile(
 );
 
 var talent_by_type_nav_template = Handlebars.compile(
-    '<ul class="nav">{{#each talent_categories}}' +
-        '<li><a href="#talents/{{toHtmlId this}}"><span data-toggle="collapse" data-target="#nav-{{toHtmlId this}}" class="dropdown collapsed"></span>{{toTitleCase this}}</a>' +
+    '<ul id="nav-talents" class="nav">{{#each talent_categories}}' +
+        '<li><a href="#talents/{{toHtmlId this}}{{currentQuery}}"><span data-toggle="collapse" data-target="#nav-{{toHtmlId this}}" class="dropdown collapsed"></span>{{toTitleCase this}}</a>' +
         '<ul class="nav collapse" id="nav-{{toHtmlId this}}">' +
         // Empty for now; will be populated later
         "</ul></li>" +
@@ -138,64 +202,197 @@ var talent_by_type_nav_template = Handlebars.compile(
 );
 
 function navTalents(tome) {
-    return talent_by_type_nav_template(tome);
+    return talent_by_type_nav_template(tome[versions.current]);
 }
 
 function fillNavTalents(tome, category) {
     var $el = $("#nav-" + category),
-        talent_types = tome.talents[category];
+        talent_types = tome[versions.current].talents[category];
     if ($.trim($el.html())) {
         // Nav already exists; no need to do more.
         return;
     }
 
     for (var i = 0; i < talent_types.length; i++) {
-        $el.append('<li><a href="#talents/' + toHtmlId(talent_types[i].type) + '">' + toTitleCase(talent_types[i].name + '</a></li>'));
+        $el.append('<li><a href="#talents/' + toHtmlId(talent_types[i].type) + currentQuery() + '">' + toTitleCase(talent_types[i].name + '</a></li>'));
         // "type" happens to be category/name, which is what we want for routing
     }
 }
 
 function listTalents(tome, category) {
-    return talent_by_type_template(tome.talents[category]);
+    return talent_by_type_template(tome[versions.current].talents[category]);
 }
 
-function initializeRoutes() {
-    // Default route.  We currently just have talents.
-    Finch.route("", function() {
-        Finch.navigate("talents");
-    });
+function configureImgSize() {
+    options.imgSize = parseInt($.cookie("imgSize") || options.imgSize);
 
-    Finch.route("talents", function() {
-        $("#side-nav").html(navTalents(tome));
-        $("#content").html("Select a talent category to the left.");
-    });
+    function showImgSizeSelection() {
+        $('.option-img-size').removeClass("selected");
+        $('.option-img-size[data-img-size="' + options.imgSize + '"]').addClass("selected");
+    }
 
-    Finch.route("[talents]/:category", function(bindings) {
-        $("#content-container").scrollTop(0);
-        loadDataIfNeeded('talents.' + bindings.category, function() {
-            var this_nav = "#nav-" + bindings.category;
-            $(this_nav).collapse('show');
-            // Hack: Update "collapsed" class, since Bootstrap doesn't seem to do it for us
-            // (unless, presumably, we use data-parent for full-blown accordion behavior,
-            // and I don't really want to do that).
-            $("[data-target=" + this_nav + "]").removeClass('collapsed');
-
-            fillNavTalents(tome, bindings.category);
-            $("#content").html(listTalents(tome, bindings.category));
-            scrollToId();
+    function changeImgSize(old_size, new_size) {
+        $("img").each(function(n, e) {
+            if ($(this).attr("width") == old_size) {
+                $(this).attr("width", new_size)
+                    .attr("height", new_size)
+                    .attr("src", $(this).attr("src").replace(old_size.toString(), new_size.toString()));
+            }
         });
-    });
+    }
 
-    Finch.route("[talents/:category]/:type", function(bindings) {
-        $("#collapse-" + bindings.type).collapse("show");
-    });
+    showImgSizeSelection();
 
-    Finch.listen();
+    $(".option-img-size").click(function(e) {
+        var old_size = options.imgSize;
+        options.imgSize = parseInt($(this).attr("data-img-size"));
+        showImgSizeSelection();
+        changeImgSize(old_size, options.imgSize);
+        $.cookie('imgSize', options.imgSize, { expires: 365, path: '/' });
+    });
+}
+
+// ToME versions.
+var versions = (function() {
+    var $_dropdown,
+        prev_expanded;
+
+    function onChange() {
+        $_dropdown.val(versions.current);
+
+        // Hack: If version changes, then save what IDs are expanded so
+        // we can restore their state after we recreate them for the
+        // new version, and also assume that the side nav needs to be
+        // refreshed.  (This is a hack because it ties the versions
+        // module too closely to our DOM organization.)
+        prev_expanded = getExpandedIds();
+        $("#side-nav").html("");
+    }
+
+    var versions = {
+        DEFAULT: '1.1.5',
+        current: '1.1.5',
+        all: [ '1.1.5', '1.2.0dev' ],
+
+        update: function(query) {
+            query = query || {};
+            query.ver = query.ver || versions.DEFAULT;
+            if (versions.current != query.ver) {
+                versions.current = query.ver;
+                onChange();
+            }
+        },
+
+        updateFinished: function() {
+            if (prev_expanded) {
+                expandIds(prev_expanded, true);
+                prev_expanded = null;
+            }
+        },
+
+        asQuery: function() {
+            if (versions.current == versions.DEFAULT) {
+                return '';
+            } else {
+                return 'ver=' + versions.current;
+            }
+        },
+
+        // Lists available versions in the given <option> element(s).
+        list: function($el, $container) {
+            var html;
+            if (versions.all.length < 2) {
+                ($container || $el).hide();
+            } else {
+                html = '';
+                for (var i = 0; i < versions.all.length; i++) {
+                    html += '<option value="' + versions.all[i] + '"';
+                    if (versions.all[i] == versions.DEFAULT) {
+                        html += ' selected';
+                    }
+                    html += '>' + versions.all[i] + '</option>';
+                }
+                ($container || $el).removeClass("hidden").show();
+                $el.html(html);
+            }
+        },
+
+        // Listens for version change events in the given <option> element(s).
+        listen: function($el) {
+            $el.change(function() {
+                versions.current = $(this).val();
+                onChange();
+                hasher.setHash(locationHashNoQuery() + currentQuery());
+            });
+        },
+
+        init: function($el, $container) {
+            $_dropdown = $el;
+            versions.list($el, $container);
+            versions.listen($el);
+        }
+    };
+    return versions;
+}());
+
+var routes;
+
+function initializeRoutes() {
+    routes = {
+
+        // Default route.  We currently just have talents.
+        default_route: crossroads.addRoute('', function() {
+            hasher.replaceHash('talents');
+        }),
+
+        talents: crossroads.addRoute('talents:?query:', function(query) {
+            versions.update(query);
+
+            if (!$("#nav-talents").length) {
+                loadDataIfNeeded('', function() {
+                    $("#side-nav").html(navTalents(tome));
+                    $("#content").html("Select a talent category to the left.");
+                });
+            }
+        }),
+
+        talents_category: crossroads.addRoute("talents/{category}:?query:", function(category, query) {
+            routes.talents.matched.dispatch(query);
+
+            $("#content-container").scrollTop(0);
+            loadDataIfNeeded('talents.' + category, function() {
+                var this_nav = "#nav-" + category;
+                showCollapsed(this_nav);
+
+                fillNavTalents(tome, category);
+                $("#content").html(listTalents(tome, category));
+                scrollToId();
+
+                versions.updateFinished();
+            });
+        }),
+
+        talents_category_type: crossroads.addRoute("talents/{category}/{type}:?query:", function(category, type, query) {
+            routes.talents_category.matched.dispatch(category, query);
+
+            $("#collapse-" + type).collapse("show");
+        })
+
+    }
+
+    function parseHash(new_hash, old_hash) {
+         crossroads.parse(new_hash);
+    }
+
+    hasher.prependHash = '';
+    hasher.initialized.add(parseHash);
+    hasher.changed.add(parseHash);
+    hasher.init();
 }
 
 function loadData(data_file, success) {
     $.ajax({
-        url: "data/" + data_file + ".json",
+        url: "data/" + versions.current + "/" + data_file + ".json",
         dataType: "json"
     }).success(success);
     // FIXME: Error handling
@@ -209,9 +406,29 @@ function loadData(data_file, success) {
  * success(tome.talents.chronomancy).
  */
 function loadDataIfNeeded(data_file, success) {
-    var parts = data_file.split("."),
-        last_part = parts.pop(),
-        tome_part = tome;
+    var parts, last_part, tome_part;
+
+    // Special case: No data has been loaded at all.
+    // Load top-level data, then reissue the request.
+    if (!tome[versions.current]) {
+        loadData('tome', function(data) {
+            tome[versions.current] = data;
+            loadDataIfNeeded(data_file, success);
+        });
+        return;
+    }
+
+    // Special case: No data file requested.
+    if (!data_file) {
+        success(tome);
+        return;
+    }
+
+    // General case: Walk the object tree to find where the requested file
+    // should go, and load it.
+    parts = data_file.split(".");
+    last_part = parts.pop();
+    tome_part = tome[versions.current];
 
     for (var i = 0; i < parts.length; i++) {
         if (typeof(tome_part[parts[i]]) === 'undefined') {
@@ -264,20 +481,20 @@ $(function() {
 
     makeStickyHeader($("#content-header"), $("#content-container"));
     enableExpandCollapseAll();
+    versions.init($(".ver-dropdown"), $(".ver-dropdown-container"));
+    configureImgSize();
 
     // Track Google Analytics as we navigate from one subpage / hash link to another.
     // Based on http://stackoverflow.com/a/4813223/25507
     // Really old browsers don't support hashchange.  A plugin is available, but I don't really care right now.
     $(window).on('hashchange', function() {
-        _gaq.push(['_trackPageview',location.pathname + location.search  + location.hash]);
+        _gaq.push(['_trackPageview', location.pathname + location.search + location.hash]);
     })
 
     // We explicitly do NOT use var, for now, to facilitate inspection in Firebug.
     // (Our route handlers and such currently also rely on tome being global.)
     tome = {};
-    loadData('tome', function(data) {
-        tome = data;
-        initializeRoutes();
-    });
+
+    initializeRoutes();
 });
 
